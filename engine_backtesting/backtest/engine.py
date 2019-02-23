46e8c974836861275885e0b0c8bd5b3a472e482e
Callam Delaney <cal.97g@gmail.com>
2019-02-23 06:08:19 +0000

    backtesting engine and draft unit tests for the engine

--- engine_backtesting/backtest/engine.py
+++ engine_backtesting/backtest/engine.py
@@ -1,14 +1,25 @@
 
 class Backtest(object):
 
-    def __init__(self, topic, from, to, algo_id, historical_context_number=30,
-        data=None, format="CANDLE"
-        ):
+    def __init__(self, topic, dt_from, dt_to, algo_id, historical_context_number=30,
+        data=None):
         """
         The backtest instance. This is the class that will drive the backtest.
 
         It knows only the id of the algorithm it will call, nothing about the
         code it will execute.
+
+        :param topic: string; the 'key' of the data the backtest will use. The
+         data it cares about.
+        :param dt_from: datetime; when to start the backtest
+        :param dt_to: datetime; when to end the backtest
+        :algo_id: uuid, string; The ID of the algorithm according to the algo
+         service.
+        :param historical_context_number: number of previous data updates to
+         include as context.
+        :param data: optional list of ABDataFormat instances, candle or tick.
+         Currently, we are only supporting candles. If this list is not provided
+         data will be queried from the TSDB (tbd).
         """
 
         self.data = data
@@ -32,7 +43,10 @@ class Backtest(object):
         pass
 
     def main(self):
-        for update in self.data[historical_context_number:-1]:
+        for i, update in enumerate(self.data[historical_context_number:-1]):
             context = {
-                "update": update
+                "update": update,
+                "context": self.data[i:i+]
+                "format": self.format
+                "topic": self.topic
             }
--- engine_backtesting/test/unit/test_engine.py
+++ engine_backtesting/test/unit/test_engine.py
@@ -1,20 +1,21 @@
 import os
 import json
 import service
+import random
+import uuid
 
 import datetime as dt
 
 import pytest
 
 from backtest.engine import Backtest
-
-@pytest.fixture
-def timeseries():
-    timeseries = []
-    return timeseries
+import core
 
 def test_run_backtest():
-    pass
+    dt_from = dt.datetime(year=2018, month=10, day=1)
+    dt_to = dt.datetime(year=2019, month=2, day=1)
+
+    bt = Backtest("GDAX:BTC-USD:5M", )
 
 def test_no_look_forward():
     pass
